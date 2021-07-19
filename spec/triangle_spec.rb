54a28630686b6d85efba41d189fa78ea02b37c3c
mireie <michael.reiersgaard@gmail.com>
2021-07-19 13:45:54 -0700

    triangle tests pass

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -7,5 +7,17 @@ describe "#Triangle" do
   it("returns not for non-valid triangle") do
     expect(triangle.type).to(eq('not'))
   end
+  triangle2 = Triangle.new(5,5,5)
+  it("returns equilateral for a triangle with equal sides") do
+    expect(triangle2.type).to(eq('equilateral'))
+  end
+  triangle3 = Triangle.new(5,5,8)
+  it("returns isosceles for a triangle with exactly 2 equal sides") do
+    expect(triangle3.type).to(eq('isosceles'))
+  end
+  triangle4 = Triangle.new(3,2,4)
+  it("returns scalene for triangle with all sides being un-equal") do
+    expect(triangle4.type).to(eq('scalene'))
+  end
 end 
 
